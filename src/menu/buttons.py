fb94b76946960eaef3a04fa454a476f7676ae4ab
im_zephyrus <97819866+franciskendrick@users.noreply.github.com>
2022-08-17 13:57:00 +0800

    Added menu modules and its images

---
+++ src/menu/buttons.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+
+class Buttons:
+    def __init__(self):
+        pass
+
+    def draw(self, display):
+        pass
---
+++ src/menu/tutorial.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+
+class Tutorial:
+    def __init__(self):
+        pass
+
+    def draw(self, display):
+        pass
